bb8dfbb74aa5587b24c1315132cee6e8c0a43552
contactochiga <contactochiga@gmail.com>
2025-11-18 19:44:33 +0100

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,8 +5,7 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('login')
-  async login(@Body() dto: { usernameOrEmail: string; password: string }) {
-    return this.authService.login(dto);
-  }
+  @Post("login")
+async login(@Body() body: { usernameOrEmail: string; password: string }) {
+  return this.authService.login(body);
 }
