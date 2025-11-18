ae840c4b439d29909554d10f97df8a63eceb575f
difusedosmosis-ship-it <difusedosmosis@gmail.com>
2025-11-18 23:56:06 +0100

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,7 +5,8 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post("login")
-async login(@Body() body: { usernameOrEmail: string; password: string }) {
-  return this.authService.login(body);
+  @Post('login')
+  async login(@Body() body: { usernameOrEmail: string; password: string }) {
+    return this.authService.login(body);
+  }
 }
