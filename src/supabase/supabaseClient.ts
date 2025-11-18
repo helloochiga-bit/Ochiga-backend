cea927bf393f58dafcdb8f7770425637e1d27061
contactochiga <contactochiga@gmail.com>
2025-11-18 14:12:18 +0100

    Update supabaseClient.ts

--- src/supabase/supabaseClient.ts
+++ src/supabase/supabaseClient.ts
@@ -3,5 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = createClient(
   process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // server-side secret
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
