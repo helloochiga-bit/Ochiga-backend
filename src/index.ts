b81ac59f26074852f8903d5cc031c9e59d02b20e
contactochiga <contactochiga@gmail.com>
2025-11-18 10:16:35 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import app from "./app";
 import { PORT } from "./config/env";
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Ochiga backend running on http://localhost:${PORT}`);
+const DEFAULT_PORT = 5000;
+
+const port = PORT || DEFAULT_PORT;
+
+const server = app.listen(port, () => {
+  console.log(`Ochiga backend running on http://localhost:${port}`);
+});
+
+server.on("error", (err: any) => {
+  if (err.code === "EADDRINUSE") {
+    console.warn(`Port ${port} is in use, trying a random free port...`);
+    server.listen(0); // 0 tells Node to pick a free port automatically
+  } else {
+    console.error(err);
+  }
 });
