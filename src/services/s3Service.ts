8e833d37a09591a79cf3cdc93c8502877c369616
helloochiga-bit <helloochiga@gmail.com>
2025-11-29 13:57:02 +0100

    Create s3Service.ts

---
+++ src/services/s3Service.ts
@@ -0,0 +1,21 @@
+import AWS from "aws-sdk";
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION,
+});
+
+export async function uploadToS3(filename: string, buffer: Buffer, mime: string) {
+  const params = {
+    Bucket: process.env.AWS_S3_BUCKET!,
+    Key: filename,
+    Body: buffer,
+    ContentType: mime,
+    ACL: "public-read",
+  };
+
+  await s3.putObject(params).promise();
+
+  return `https://${process.env.AWS_S3_BUCKET}.s3.amazonaws.com/${filename}`;
+}
